08f9e6f26ae26b35873c5c86ebb9b899ff4ad43d
Tyler Durden <98093195+WhoIsTylerDurden@users.noreply.github.com>
2022-06-30 22:57:52 +0300

    Add files via upload

---
+++ Files.cpp
@@ -0,0 +1,60 @@
+#include "Files.h"
+#include <random>
+
+using namespace std;
+
+void ReadCSV(const string& filename, vector<RowVector*>& data)
+{
+    data.clear();
+    ifstream file(filename);
+    string line, word;
+    getline(file, line, '\n'); // кількість стовпчиків у файлі
+    stringstream ss(line);
+    vector<Scalar> parsed_vector;
+    while (getline(ss, word, ','))
+    {
+        // розбираємо вираз на складові за розділовим символом
+        parsed_vector.push_back(Scalar(stof(&word[0])));
+    }
+    uint cols = parsed_vector.size();
+    data.push_back(new RowVector(cols));
+    for (int i = 0; i < cols; i++)
+    {
+        data.back()->coeffRef(1, i) = parsed_vector[i];
+    }
+
+    // читання файлу
+    if (file.is_open())
+    {
+        while (getline(file, line, '\n'))
+        {
+            stringstream string_line(line);
+            data.push_back(new RowVector(1, cols));
+            int i = 0;
+            while (getline(string_line, word, ','))
+            {
+                data.back()->coeffRef(i) = Scalar(stof(&word[0]));
+                i++;
+            }
+        }
+    }
+}
+
+// генерація відносно випадкових даних
+void genData(const string& filename, int lines)
+{
+    ofstream file1(filename + "-in");
+    ofstream file2(filename + "-out");
+    for (int r = 0; r < lines; r++)
+    {
+        Scalar x =  rand() / Scalar(RAND_MAX);
+        Scalar y = rand() / Scalar(RAND_MAX);
+        file1 << x << "," << y << endl;
+        // будь-яка зміна даних, яку буде передбачувати нейронна мережа
+        file2 << 22 * x + sqrt(y) << endl;
+    }
+    file1.close();
+    file2.close();
+}
+
+
