de2a8c0b00e465d67396fd9539ce77e19f495af1
Раян Госленко <olkhovatyi@knu.ua>
2022-10-15 20:08:36 +0300

    Added cmake

---
+++ NeuralNetwork/cmake-build-debug/CMakeFiles/NeuralNetwork.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NeuralNetwork.dir/Files.cpp.o"
+  "CMakeFiles/NeuralNetwork.dir/NeuralNetwork.cpp.o"
+  "CMakeFiles/NeuralNetwork.dir/main.cpp.o"
+  "NeuralNetwork"
+  "NeuralNetwork.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NeuralNetwork.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
